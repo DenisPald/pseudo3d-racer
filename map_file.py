8e60154baa0fc49d15a0541dffafab50f172fad9
DenisPald <masgutovmihail@gmail.com>
2022-01-20 18:27:57 +0500

    redesign config and map file

--- map_file.py
+++ map_file.py
@@ -1,30 +1,30 @@
 import random
 
-TOTAL_SEGMENTS = 500
-MAP = []
+def remake_map(total_segments, step):
+    res = []
+    n_of_steps = total_segments // step
 
-STEP = 100
-N_OF_STEPS = TOTAL_SEGMENTS // STEP
+    dx = [0, ]
+    dy = [0, ]
+    for i in range(1, n_of_steps - 1):
+        cur_dy = random.randint(0, 0)
+        cur_dx = random.randint(-0, 0)
 
-dx = [0, ]
-dy = [0, ]
-for i in range(1, N_OF_STEPS - 1):
-    cur_dy = random.randint(0, 0)
-    cur_dx = random.randint(-0, 0)
+        if sum(dy) < 0:
+            dy.append(cur_dy)
+        else:
+            dy.append(-cur_dy)
 
-    if sum(dy) < 0:
-        dy.append(cur_dy)
-    else:
-        dy.append(-cur_dy)
+        if sum(dx) < 150:
+            dx.append(cur_dx)
+        else:
+            dx.append(-cur_dx)
 
-    if sum(dx) < 150:
-        dx.append(cur_dx)
-    else:
-        dx.append(-cur_dx)
+    dx.append(-sum(dx))
+    dy.append(-sum(dy))
 
-dx.append(-sum(dx))
-dy.append(-sum(dy))
+    for i in range(n_of_steps):
+        for j in range(step):
+            res.append((dx[i], dy[i]))
 
-for i in range(N_OF_STEPS):
-    for j in range(STEP):
-        MAP.append((dx[i], dy[i]))
+    return res
